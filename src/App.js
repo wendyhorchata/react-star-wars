cc7a735856995ff2c6cf7f8f1a0f5f9d50f03a08
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-05-15 18:27:08 -0700

    stocks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
 
-function App() {
+ const getStarship = (setStarship) => fetch("https://swapi.dev/api/starships/")
+  .then(response => response.json())
+  .then(setStarship)
+  
+   function App(props) {
+
+  const [starship, setStarship] = useState([])
+useEffect(() =>{getStarship(setStarship)}, [])
+
+if(starship.length <= 0) {
+  return <h2>Hold On</h2> 
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="Starships">Starwars StarShips</h1>
+      <div className='container'>
+      {starship?.results.map((ship, index)=>{
+        return <div className='ship' key={index}>{ship.name}</div>
+      })
+    }
+      
+        </div>
     </div>
-  );
+  )
+  
 }
+
 
 export default App;
